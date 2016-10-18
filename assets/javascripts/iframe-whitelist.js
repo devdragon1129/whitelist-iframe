15a507011d3e63a57e458ffd6c115f447bcbd1d2
tolstoshev <nicholas@growersnetwork.org>
2016-10-18 15:35:41 -0600

    Update iframe-whitelist.js

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -1,6 +1,6 @@
 (function() {
   if (Discourse.dialect_deprecated) { return; }
 
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.surveygizmo\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.surveygizmo\.com\/s3\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
 })();
