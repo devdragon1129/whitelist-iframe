fde11abc0ef83afdbd1d4166e22ab50accc982ce
Nazarii <devdragon1129@gmail.com>
2023-02-14 22:48:54 +0800

    add iframe srcs to iframe-whitelists

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -1,6 +1,7 @@
 (function() {
   if (Discourse.dialect_deprecated) { return; }
 
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.surveygizmo\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/nazarii-cbf\/.cashbackforex\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/nazarii-rkfx\/.cashbackforex\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
 })();
